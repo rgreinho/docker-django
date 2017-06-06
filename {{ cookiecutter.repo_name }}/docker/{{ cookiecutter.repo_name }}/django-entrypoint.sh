6eb0702da52e73d711aded9a4e7bba764cbf4e81
Rémy Greinhofer <regreinh@cisco.com>
2017-06-06 14:52:41 -0500

    Update with better practices and newer versions
    * Improve general features of the Makefile
    * Improve Makefile for Docker usage
    * Improve docker-compose setup
    * Update pip package versions

--- {{ cookiecutter.repo_name }}/docker/{{ cookiecutter.repo_name }}/django-entrypoint.sh
+++ {{ cookiecutter.repo_name }}/docker/{{ cookiecutter.repo_name }}/django-entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+set -euo pipefail
+
+# Define variables.
+: ${{ '{' }}{{ cookiecutter.prefix|upper }}_{{ cookiecutter.repo_name }}_API_PORT:=8000}
 
 # Ensure the DB is up and running before starting Django.
->&2 echo -n "Starting Celery Flower."
-until psql -h "db" -U "postgres" -c '\l'; do
+>&2 echo -n "Waiting for DB to be up and running."
+until psql -h "{{ cookiecutter.prefix|lower }}-{{ cookiecutter.repo_name }}-db" -U "postgres" -c '\l'; do
   sleep 1
   >&2 echo -n .
 done
@@ -11,4 +15,4 @@ done
 python manage.py migrate
 
 # Start development server on public ip interface, on port 8000.
-exec gunicorn --reload -b 0.0.0.0:8000 {{ cookiecutter.repo_name }}.wsgi
+exec gunicorn --reload -b 0.0.0.0:${{ '{' }}{{ cookiecutter.prefix|upper }}_{{ cookiecutter.repo_name|upper }}_API_PORT} {{ cookiecutter.repo_name }}.wsgi
