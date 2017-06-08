f210f050affd537c4683937b194bdbdf747bfb75
Rémy Greinhofer <regreinh@cisco.com>
2017-06-08 15:22:31 -0500

    Clean up Django settings

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name }}/settings/base.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name }}/settings/base.py
@@ -30,18 +30,15 @@ DJANGO_APPS = [
     'django.contrib.messages',
     'django.contrib.sessions',
     'django.contrib.staticfiles',
-]  # yapf: disable
+]
 
 # 3rd party apps.
-THIRD_PARTY_APPS = [
-    'djcelery',
-    'kombu.transport.django',
-]  # yapf: disable
+THIRD_PARTY_APPS = []
 
 # Project applications.
 PROJECT_APPS = [
     '{{ cookiecutter.django_app }}',
-]  # yapf: disable
+]
 
 # Installed apps is a combination of all the apps.
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
@@ -62,9 +59,7 @@ ROOT_URLCONF = '{{ cookiecutter.repo_name }}.urls'
 
 # Define the site admins.
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#admins
-ADMINS = (
-    ('{{ cookiecutter.author }}', '{{ cookiecutter.author_email }}'),
-)  # yapf: disable
+ADMINS = (('{{ cookiecutter.author }}', '{{ cookiecutter.author_email }}'), )
 
 # Define site managers.
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#managers
@@ -90,9 +85,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files.
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
-STATICFILES_DIRS = (
-    root('assets'),
-)  # yapf: disable
+STATICFILES_DIRS = (root('assets'), )
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
@@ -100,27 +93,24 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Templates configuration.
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#templates
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'APP_DIRS': True,
-        'DIRS': [
-            root('templates'),
+TEMPLATES = [{
+    'BACKEND': 'django.template.backends.django.DjangoTemplates',
+    'APP_DIRS': True,
+    'DIRS': [
+        root('templates'),
+    ],
+    'OPTIONS': {
+        'context_processors': [
+            'django.contrib.auth.context_processors.auth',
+            'django.template.context_processors.debug',
+            'django.template.context_processors.i18n',
+            'django.template.context_processors.media',
+            'django.template.context_processors.static',
+            'django.template.context_processors.tz',
+            'django.contrib.messages.context_processors.messages',
         ],
-        'OPTIONS': {
-            'debug': DEBUG,
-            'context_processors': [
-                'django.contrib.auth.context_processors.auth',
-                'django.template.context_processors.debug',
-                'django.template.context_processors.i18n',
-                'django.template.context_processors.media',
-                'django.template.context_processors.static',
-                'django.template.context_processors.tz',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    }
-]  # yapf: disable
+    },
+}]
 
 # Password validation.
 AUTH_PASSWORD_VALIDATORS = [
@@ -145,7 +135,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}  # yapf: disable
+}
 
 # Update database configuration with ${DATABASE_URL}.
 db_from_env = dj_database_url.config(conn_max_age=500)
@@ -164,11 +154,3 @@ if os.environ.get('REDIS_URL'):
             }
         }
     }
-
-# Celery configuration.
-BROKER_URL = os.environ.get('CELERY_BROKER_URL', 'django://')
-CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND', 'djcelery.backends.database:DatabaseBackend')
-CELERY_ACCEPT_CONTENT = ['application/json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TIMEZONE = TIME_ZONE
