ea2c54bb0f246d5fe56bbfdc75ad2b33673fc463
Rémy Greinhofer <remy.greinhofer@gmail.com>
2017-06-10 11:20:08 -0500

    Fix Celery entrypoint

--- {{ cookiecutter.repo_name }}/docker/{{ cookiecutter.repo_name }}/celery-entrypoint.sh
+++ {{ cookiecutter.repo_name }}/docker/{{ cookiecutter.repo_name }}/celery-entrypoint.sh
@@ -4,8 +4,8 @@
 CELERY_COMMAND=${1:-worker}
 
 # Define variables.
-: ${{ '{' }}{{ cookiecutter.prefix|upper }}_{{ cookiecutter.repo_name|upper }}_CELERY_APP:-{{ cookiecutter.repo_name }}.celery}
-: ${{ '{' }}{{ cookiecutter.prefix|upper }}_{{ cookiecutter.repo_name|upper }}_CELERY_LOG_LEVEL:-info}
+: ${{ '{' }}{{ cookiecutter.prefix|upper }}_{{ cookiecutter.repo_name|upper }}_CELERY_APP:={{ cookiecutter.repo_name }}.celery}
+: ${{ '{' }}{{ cookiecutter.prefix|upper }}_{{ cookiecutter.repo_name|upper }}_CELERY_LOG_LEVEL:=info}
 
 # Start Celery command.
 DATE=$(date -u +%Y%m%dT%H%M%S%Z)
