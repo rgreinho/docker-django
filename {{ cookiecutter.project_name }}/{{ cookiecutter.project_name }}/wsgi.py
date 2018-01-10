d7d6945ee6ce9a7f2add133262a049ef46b6631b
Rémy Greinhofer <rgreinhofer@rexchange.com>
2018-01-10 15:25:31 -0600

    Refresh this cookiecutter
    * Use setuptools extras to manage dependencies
    * Update dependencies
    * Update cookiecutter parameters to make unattended install possible
    * Update Makefile
    * Add a Makefile for the cookicutter itself
    * Implement CircleCi

---
+++ {{ cookiecutter.project_name }}/{{ cookiecutter.project_name }}/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for {{ cookiecutter.project_name }} project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+"""
+import os
+from os.path import abspath, dirname
+from sys import path
+
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
+SITE_ROOT = dirname(dirname(abspath(__file__)))
+path.append(SITE_ROOT)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ cookiecutter.project_name }}.settings")
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
--- {{ cookiecutter.repo_name }}/{{ cookiecutter.repo_name }}/celery/celery.py
+++
@@ -1,8 +0,0 @@
-"""Create the Celery app."""
-from celery import Celery
-
-# Celery application.
-app = Celery()
-app.config_from_object('{{ cookiecutter.repo_name }}.celery.celery_settings')
-# TODO(remyg): This does not seem to work.
-app.autodiscover_tasks(['{{ cookiecutter.repo_name }}.celery'])
